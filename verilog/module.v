537753ade6b52b2ccb34e80095e4031a75e607ea
rishucoding <rishucoding@gmail.com>
2018-07-05 01:21:54 +0530

    Revert "First commit"
    This reverts commit 94199b8f974d426e40b870ced723d31f5212fe48.

--- verilog/module.v
+++
@@ -1,8 +0,0 @@
-module equality(out, in1, in2);
-    input [1:0 ]in1;
-    input in2;
-    output out;
-   // assign in1 = 2'b10;
-   // assign in2 = 1;
-    assign out = in1 == 2'b10 && in2;
-endmodule
